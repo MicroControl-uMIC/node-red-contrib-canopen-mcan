3a5ddf274e5dd5299d96959c9c370b336387c8fc
putzka <putzka@192.168.1.32>
2019-11-06 13:13:23 +0100

    changed all names of modules removing the channel number

---
+++ canopen/mcan-di.js
@@ -0,0 +1,146 @@
+/*jshint esversion: 6 */ 
+
+//-----------------------------------------------------------------------------------------------------//
+// for detailed information: https://nodered.org/docs/creating-nodes/node-js                           //
+//-----------------------------------------------------------------------------------------------------//
+
+//-----------------------------------------------------------------------------------------------------
+// import packages using "require" here
+//-----------------------------------------------------------------------------------------------------
+
+
+const DeviceIdString  = require("./core/id_string");
+const WsComet = require("./core/websocket_comet.js");
+const NodeData  = require("./core/node_data.js");
+const NodeErrorEnum	  = require("./core/node_error.js");
+
+const moduledeviceType     = 197009;
+const moduleProductCode    = 1286014;
+const moduleRevisionNumber = 2;
+
+var di_socket;
+
+var diStateOld = false;
+
+var node;
+
+//-----------------------------------------------------------------------------------------------------
+// define variables here
+//-----------------------------------------------------------------------------------------------------
+
+module.exports = function(RED) {
+	
+    class MCANDi
+    {
+		constructor(config) 
+	    {
+	        RED.nodes.createNode(this,config);
+	       
+	        //---------------------------------------------------------------------------------------------
+	        // runs when flow is deployed
+	        //---------------------------------------------------------------------------------------------
+	        node = this;  
+	        node.on('close', node.close);
+	        
+	        node.nodeId 		= config.nodeId;
+	        node.productCode 	= config.productCode;
+	        node.canBus 		= config.canBus;
+	        node.moduleChannel  = config.moduleChannel;
+	
+	        //create Buffer for rcv Data
+	        var di_data = new NodeData();
+	        
+	        //creat id String
+	        var identification = new DeviceIdString(node.canBus, node.nodeId, node.moduleChannel, 
+					14, moduleProductCode , moduleRevisionNumber, moduledeviceType);
+	        
+	         //add specific string
+	        var idString = identification.getIdString();
+	        idString = idString + "port-direction: 0"+ ";"; 
+	        
+	        //open socket
+	        di_socket = new WsComet(node.canBus, node.nodeId, node.moduleChannel);       
+	        
+			var client = di_socket.connect_ws();
+	        
+			client.onopen = function()
+			{
+				//send identification string upon socket connection
+	    	    console.log(idString);
+				client.send(idString);
+			};
+			
+	    	client.onclose = function() 
+	    	{
+	    	    console.log('echo-protocol Client Closed');
+	    	    node.status({fill:"red",shape:"dot",text: "[In "+di_socket.getChannelUrl()+"] Not connected"});
+	    	};
+	    	
+	        //gets executed when socket receives a message	
+	    	client.onmessage = function (event) 
+	    	{
+	    			//console.log("msg received");
+	
+	    			di_data.setBuffer(event.data, 32);
+	       
+	                //check Status Variable
+	                if(di_data.getValue(1) === NodeErrorEnum.eNODE_ERR_NONE)
+	            	{
+	                	node.status({fill:"green",shape:"dot",text: "[In "+di_socket.getChannelUrl()+"] OK"});
+	                	
+	                	var diState = di_data.getValue(0);
+
+	                	if(diState != diStateOld)
+	                	{
+		                	var msgData = {payload: diState ,
+	                				       topic: "mcan8dio/" + node.moduleChannel};
+	                		diStateOld = diState;
+		                	node.send(msgData);
+	                	}
+	                	
+	            	}
+	                else if(di_data.getValue(1) === NodeErrorEnum.eNODE_ERR_SENROR)
+	            	{
+	                	node.status({fill:"yellow",shape:"dot",text: "[In "+di_socket.getChannelUrl()+"] Error"});                	
+	            	}	                
+	                else if(di_data.getValue(1) === NodeErrorEnum.eNODE_ERR_COMMUNICATION)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+di_socket.getChannelUrl()+"] Error"});
+	            	}
+	                else if(di_data.getValue(1) === NodeErrorEnum.eNODE_ERR_CONNECTION)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+di_socket.getChannelUrl()+"] Not connected"});
+	            	}
+	                else if(di_data.getValue(1) === NodeErrorEnum.eNODE_ERR_CONNECTION_NETWORK)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+di_socket.getChannelUrl()+"] Wrong Network"});
+	            	}
+	                else if(di_data.getValue(1) === NodeErrorEnum.eNODE_ERR_CONNECTION_DEVICE)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+di_socket.getChannelUrl()+"] Wrong Node-ID"});
+	            	}
+	                else if(di_data.getValue(1) === NodeErrorEnum.eNODE_ERR_CONNECTION_CHANNEL)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+di_socket.getChannelUrl()+"] Wrong Channel"});
+	            	}
+	                else if(ti_data.getValue(1) === NodeErrorEnum.eNODE_ERR_DEVICE_IDENTIFICATION)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+ti_socket.getChannelUrl()+"] Wrong device identification"});
+	            	}
+	                
+	    		};
+	    }
+		
+        //---------------------------------------------------------------------------------------------
+        // runs when node is closed (before deploy, e.g. to tidy up)
+        //---------------------------------------------------------------------------------------------
+        close()
+        {
+        	di_socket.disconnect_ws();
+        	node.status({fill:"red",shape:"dot",text: "[In "+di_socket.getChannelUrl()+"] Not connected"});
+        }
+
+    }
+    
+    RED.nodes.registerType("mcan-dio in", MCANDi);
+}
---
+++ canopen/mcan-ti.js
@@ -0,0 +1,148 @@
+/*jshint esversion: 6 */ 
+'use strict';
+//-----------------------------------------------------------------------------------------------------//
+// for detailed information: https://nodered.org/docs/creating-nodes/node-js       
+//-----------------------------------------------------------------------------------------------------//
+
+//-----------------------------------------------------------------------------------------------------
+// import packages using "require" here
+//-----------------------------------------------------------------------------------------------------
+
+
+const DeviceIdString  = require("./core/id_string");
+const WsComet         = require("./core/websocket_comet.js");
+const NodeData        = require("./core/node_data.js");
+const NodeErrorEnum	  = require("./core/node_error.js");
+
+const moduledeviceType     = 131476;
+const moduleProductCode    = 1243005;
+const moduleRevisionNumber = 1;
+
+
+
+
+//-----------------------------------------------------------------------------------------------------
+// define variables here
+//-----------------------------------------------------------------------------------------------------
+
+module.exports = function(RED) {
+	
+    class MCANTi
+    {
+		constructor(config) 
+	    {
+	        RED.nodes.createNode(this,config);
+	       
+	       //---------------------------------------------------------------------------------------------
+	       // runs when flow is deployed
+	       //---------------------------------------------------------------------------------------------
+	       const node = this;  
+		   node.on('close', node.close);
+
+		   	//this is neccassary to store objects within node to access it in other functions
+			const context = node.context();
+
+
+           var canBus        = config.canBus;
+           var nodeId        = config.nodeId;
+		   var moduleChannel = config.moduleChannel;
+		   
+		   var sensorType    = config.sensorType;
+
+	        //create Buffer for rcv Data
+	        const tiData = new NodeData();
+
+	        //creat id String
+			  var identification = new DeviceIdString(canBus, nodeId, moduleChannel, 
+																	14, moduleProductCode , moduleRevisionNumber, moduledeviceType);
+	        //add specific string
+			  var idString = identification.getIdString();
+			  idString = idString + "sensor-type: "    + sensorType + ";";
+			
+	        //open socket
+	        const tiSocket = new WsComet(canBus, nodeId, moduleChannel);       
+	        
+			const client = tiSocket.connect_ws();
+
+	        //store the client in the context of node
+			context.set('client', client);
+
+			client.onopen = function()
+			{
+				//send identification string upon socket connection
+				console.log(idString);
+				client.send(idString);
+			};
+			
+	    	client.onclose = function() 
+	    	{
+	    	    console.log('echo-protocol Client Closed');
+	    	    node.status({fill:"red",shape:"dot",text: "[In "+moduleChannel+"] Not connected"});
+	    	};
+	    	
+	        //gets executed when socket receives a message	
+	    	client.onmessage = function (event) 
+	    	{
+				tiData.setBuffer(event.data, 32);
+
+	                //check Status Variable
+	                if(tiData.getValue(1) === NodeErrorEnum.eNODE_ERR_NONE)
+	            	{
+	                	node.status({fill:"green",shape:"dot",text: "[In "+moduleChannel+"] OK"});
+	                	let scaledData = tiData.getValue(0) / 10;
+	                	const msgData = {payload: scaledData ,
+										topic: "mcan4ti/" + moduleChannel};
+
+
+						node.send(msgData);	                	
+	            	}
+	                else if(tiData.getValue(1) === NodeErrorEnum.eNODE_ERR_SENROR)
+	            	{
+	                	node.status({fill:"yellow",shape:"dot",text: "[In "+moduleChannel+"] Error"});                	
+	            	}	                
+	                else if(tiData.getValue(1) === NodeErrorEnum.eNODE_ERR_COMMUNICATION)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+moduleChannel+"] Error"});
+	            	}
+	                else if(tiData.getValue(1) === NodeErrorEnum.eNODE_ERR_CONNECTION)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+moduleChannel+"] Not connected"});
+	            	}
+	                else if(tiData.getValue(1) === NodeErrorEnum.eNODE_ERR_CONNECTION_NETWORK)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+moduleChannel+"] Wrong Network"});
+	            	}
+	                else if(tiData.getValue(1) === NodeErrorEnum.eNODE_ERR_CONNECTION_DEVICE)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+moduleChannel+"] Wrong Node-ID"});
+	            	}
+	                else if(tiData.getValue(1) === NodeErrorEnum.eNODE_ERR_CONNECTION_CHANNEL)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+moduleChannel+"] Wrong Channel"});
+	            	}
+	                else if(tiData.getValue(1) === NodeErrorEnum.eNODE_ERR_DEVICE_IDENTIFICATION)
+	            	{
+	                	node.status({fill:"red",shape:"dot",text: "[In "+moduleChannel+"] Wrong device identification"});
+	            	}
+	                
+	    		};
+	    }
+
+        //---------------------------------------------------------------------------------------------
+        // runs when node is closed (before deploy, e.g. to tidy up)
+        //---------------------------------------------------------------------------------------------
+        close()
+        {
+			//neccassary to access context storage
+			var context = this.context();
+
+			//read context variable
+			const client = context.get('client');
+
+			client.close();
+		}
+
+    }
+    
+    RED.nodes.registerType("mcan-ti", MCANTi);
+}
